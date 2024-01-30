cdcc46dd66aaacb0ff793149da450582321df693
Xiaosong Bi <bixiaosong@gmail.com>
2024-01-30 18:27:01 +0100

    Refactor Modal Component for Improved Accessibility and State Sync
    - Implemented useEffect hook in Modal component to synchronize the external 'opened' prop with the internal state. This ensures that changes to the 'opened' prop in the parent component are reflected in the Modal's visibility.
    - Modified the rendering of the Content prop to treat it as a React component. Changed its PropTypes validation from node to elementType, allowing for correct component rendering.
    - Added an aria-label attribute to the close button for better accessibility. This resolves the ESLint accessibility error regarding form controls needing an associated text label, particularly beneficial for screen reader users.
    - Updated related comments and documentation to reflect these changes, ensuring clarity and maintainability of the code.

--- src/containers/Modal/index.js
+++ src/containers/Modal/index.js
@@ -1,30 +1,38 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
-import Icon from "../../components/Icon";
-import "./style.scss";
+import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
+import Icon from '../../components/Icon'
+import './style.scss'
 
 const Modal = ({ opened, Content, children }) => {
-  const [isOpened, setIsOpened] = useState(opened);
+  const [isOpened, setIsOpened] = useState(opened)
+
+  // Use useEffect to synchronize the external 'opened' state
+  useEffect(() => {
+    setIsOpened(opened)
+  }, [opened])
+
   return (
     <>
       {children({ isOpened, setIsOpened })}
       {isOpened && (
-        <div className="modal">
-          <div className="content">
-            {Content}
+        <div className='modal'>
+          <div className='content'>
+            {/* Ensure Content is rendered as a component */}
+            <Content />
             <button
-              type="button"
-              data-testid="close-modal"
+              type='button'
+              data-testid='close-modal'
               onClick={() => setIsOpened(false)}
+              aria-label='Close' // Accessible label for screen readers
             >
-              <Icon name="close" />
+              <Icon name='close' />
             </button>
           </div>
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
 Modal.defaultProps = {
   opened: false,
@@ -32,8 +40,8 @@ Modal.defaultProps = {
 
 Modal.propTypes = {
   opened: PropTypes.bool,
-  Content: PropTypes.node.isRequired,
+  Content: PropTypes.elementType.isRequired, // Updated to elementType
   children: PropTypes.func.isRequired,
 }
 
-export default Modal;
+export default Modal

2bfae093e6053c6b4f8e11eb423c738d38f3c954
Xiaosong Bi <bixiaosong@gmail.com>
2024-02-07 20:41:50 +0100

    Correct fireEvent usage for submit button click in Home component tests

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,44 +1,39 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import Home from "./index";
+import { fireEvent, render, screen } from '@testing-library/react'
+import Home from './index'
 
-describe("When Form is created", () => {
-  it("a list of fields card is displayed", async () => {
-    render(<Home />);
-    await screen.findByText("Email");
-    await screen.findByText("Nom");
-    await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
-  });
+describe('When Form is created', () => {
+  it('a list of fields card is displayed', async () => {
+    render(<Home />)
+    await screen.findByText('Email')
+    await screen.findByText('Nom')
+    await screen.findByText('Prénom')
+    await screen.findByText('Personel / Entreprise')
+  })
+  describe('and a click is triggered on the submit button', () => {
+    it('the success message is displayed', async () => {
+      render(<Home />)
+      // Await for the button to be in the document
+      const submitButton = await screen.findByText('Envoyer')
+      // Trigger the click event on the submit button
+      fireEvent.click(submitButton)
+      // Wait for the expected text to appear in the document
+      await screen.findByText('En cours')
+      await screen.findByText('Message envoyé !')
+    })
+  })
+})
 
-  describe("and a click is triggered on the submit button", () => {
-    it("the success message is displayed", async () => {
-      render(<Home />);
-      fireEvent(
-        await screen.findByText("Envoyer"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
-    });
-  });
-
-});
-
-
-describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
+describe('When a page is created', () => {
+  it('a list of events is displayed', () => {
     // to implement
   })
-  it("a list a people is displayed", () => {
+  it('a list a people is displayed', () => {
     // to implement
   })
-  it("a footer is displayed", () => {
+  it('a footer is displayed', () => {
     // to implement
   })
-  it("an event card, with the last event, is displayed", () => {
+  it('an event card, with the last event, is displayed', () => {
     // to implement
   })
-});
+})
